eca5d92398d63c3b8eccec7325b4c5be2613ce6c
FuseTim <fusetim@gmx.com>
2020-08-29 15:56:58 +0200

    feat: add generic for problem01

--- build.sc
+++ build.sc
@@ -3,3 +3,7 @@ import mill._, scalalib._
 object problem01 extends ScalaModule {
     def scalaVersion = "2.13.1"
 }
+
+object problem02 extends ScalaModule {
+    def scalaVersion = "2.13.1"
+}
--- problem01/src/main.scala
+++ problem01/src/main.scala
@@ -5,11 +5,11 @@ object Main extends App {
   // Some testing values:
   assert(last(List(1, 2, 3, 4, 5, 6)) == 6)
   assert(last(List(7, 2, 115, -85, 65, 87)) == 87)
-  assert(last(List(7, 2, 115, -85, 4)) == 4)
+  assert(last(List(7.2, 2.4, 115.4, -855.5, 4.7)) == 4.7)
   assert(last(List(1, 1, 2, 3, 5, 8)) == 8)
 
   /// Return the last element from a list
-  def last(list: List[Int]): Int = {
+  def last[T](list: List[T]): T = {
     list match {
       case Nil         => throw new NullPointerException
       case head :: Nil => return head
