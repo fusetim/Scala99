75b03b80ad4ca7cf02531c38831684abef9797b3
FuseTim <fusetim@gmx.com>
2020-08-29 16:13:56 +0200

    feat: add solve for problem02

---
+++ problem02/src/main.scala
@@ -0,0 +1,19 @@
+package fr.fusetim.scala99.problem02
+
+object Main extends App {
+  // Some testing values:
+  assert(penultimate(List(1, 2, 3, 4, 5, 6)) == 5)
+  assert(penultimate(List(7, 2, 115, -85, 65, 87)) == 65)
+  assert(penultimate(List(7.2, 2.4, 115.4, -855.5, 4.7)) == -855.5)
+  assert(penultimate(List(1, 1, 2, 3, 5, 8)) == 5)
+
+  /// Return the penultimate element from a list
+  def penultimate[T](list: List[T]): T = {
+    list match {
+        case Nil => throw new NullPointerException
+        case head :: Nil => throw new NullPointerException
+        case head :: (body :: Nil) => return head
+        case head :: tail => return penultimate(tail)
+    }
+  }
+}
